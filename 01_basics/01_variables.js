c9735ad86334f18847fbad7818b74922ba0f33ad
Zeeshan0610 <120152689+Zeeshan0610@users.noreply.github.com>
2024-02-14 13:53:14 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "zeeshan@google.com"
+var accountPassword = "12345"
+accountCity = "Bengalore"
+
+//accountId = 2
+accountEmail = "mukram@google.com"
+accountPassword = "54321"
+accountCity = "Hubli" 
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we r using nodejs, not  browser
+
+console.log(3 + 3);
+
+let name = "Zeeshan"
+let age = 22
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof "3+3");
